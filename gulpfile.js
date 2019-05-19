472ca4d1229a5c1e1de8367e897bda8995bfdd98
aizadazh <46402847+aizadazh@users.noreply.github.com>
2019-05-19 19:16:55 +0600

    header done

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
  
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+gulp.task("sass", function() {
+  return gulp
+      .src("./sass/style.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("./dist/css"));
 });
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+
+gulp.task("sass:watch", function() {
+  gulp.watch("./sass/**/*.scss", gulp.series("sass"));
 });
